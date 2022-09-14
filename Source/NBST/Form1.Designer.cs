eed07cbc1e735c7a0da5d6f811e9cb9373d3d93c
SAMPI Dev Kit <sampidevkit@gmail.com>
2022-09-14 23:50:16 +0700

    initialize Win32 Modem test tool

---
+++ Source/NBST/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace NBST
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tabCtrl1 = new System.Windows.Forms.TabControl();
+            this.tabGraph = new System.Windows.Forms.TabPage();
+            this.zedGraph = new ZedGraph.ZedGraphControl();
+            this.tabLog = new System.Windows.Forms.TabPage();
+            this.rtb_Log = new System.Windows.Forms.RichTextBox();
+            this.rtb_Info = new System.Windows.Forms.RichTextBox();
+            this.bt_Scan = new System.Windows.Forms.Button();
+            this.bt_RFTest = new System.Windows.Forms.Button();
+            this.cb_Port1 = new System.Windows.Forms.ComboBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.bt_Download = new System.Windows.Forms.Button();
+            this.tabCtrl1.SuspendLayout();
+            this.tabGraph.SuspendLayout();
+            this.tabLog.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabCtrl1
+            // 
+            this.tabCtrl1.Controls.Add(this.tabGraph);
+            this.tabCtrl1.Controls.Add(this.tabLog);
+            this.tabCtrl1.Location = new System.Drawing.Point(12, 12);
+            this.tabCtrl1.Name = "tabCtrl1";
+            this.tabCtrl1.SelectedIndex = 0;
+            this.tabCtrl1.Size = new System.Drawing.Size(800, 449);
+            this.tabCtrl1.TabIndex = 3;
+            // 
+            // tabGraph
+            // 
+            this.tabGraph.BackColor = System.Drawing.Color.Transparent;
+            this.tabGraph.Controls.Add(this.zedGraph);
+            this.tabGraph.Location = new System.Drawing.Point(4, 22);
+            this.tabGraph.Name = "tabGraph";
+            this.tabGraph.Padding = new System.Windows.Forms.Padding(3);
+            this.tabGraph.Size = new System.Drawing.Size(792, 423);
+            this.tabGraph.TabIndex = 0;
+            this.tabGraph.Text = "Graph";
+            // 
+            // zedGraph
+            // 
+            this.zedGraph.BackColor = System.Drawing.SystemColors.Info;
+            this.zedGraph.Location = new System.Drawing.Point(6, 6);
+            this.zedGraph.Name = "zedGraph";
+            this.zedGraph.ScrollGrace = 0D;
+            this.zedGraph.ScrollMaxX = 0D;
+            this.zedGraph.ScrollMaxY = 0D;
+            this.zedGraph.ScrollMaxY2 = 0D;
+            this.zedGraph.ScrollMinX = 0D;
+            this.zedGraph.ScrollMinY = 0D;
+            this.zedGraph.ScrollMinY2 = 0D;
+            this.zedGraph.Size = new System.Drawing.Size(780, 411);
+            this.zedGraph.TabIndex = 3;
+            this.zedGraph.UseExtendedPrintDialog = true;
+            // 
+            // tabLog
+            // 
+            this.tabLog.BackColor = System.Drawing.Color.Transparent;
+            this.tabLog.Controls.Add(this.rtb_Log);
+            this.tabLog.Location = new System.Drawing.Point(4, 22);
+            this.tabLog.Name = "tabLog";
+            this.tabLog.Padding = new System.Windows.Forms.Padding(3);
+            this.tabLog.Size = new System.Drawing.Size(792, 423);
+            this.tabLog.TabIndex = 1;
+            this.tabLog.Text = "Log";
+            // 
+            // rtb_Log
+            // 
+            this.rtb_Log.BackColor = System.Drawing.SystemColors.Info;
+            this.rtb_Log.Location = new System.Drawing.Point(6, 6);
+            this.rtb_Log.Name = "rtb_Log";
+            this.rtb_Log.Size = new System.Drawing.Size(964, 411);
+            this.rtb_Log.TabIndex = 0;
+            this.rtb_Log.Text = "";
+            this.rtb_Log.TextChanged += new System.EventHandler(this.rtb_Log_TextChanged);
+            this.rtb_Log.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.rtb_Log_MouseDoubleClick);
+            // 
+            // rtb_Info
+            // 
+            this.rtb_Info.Location = new System.Drawing.Point(818, 71);
+            this.rtb_Info.Name = "rtb_Info";
+            this.rtb_Info.Size = new System.Drawing.Size(178, 380);
+            this.rtb_Info.TabIndex = 6;
+            this.rtb_Info.Text = "";
+            // 
+            // bt_Scan
+            // 
+            this.bt_Scan.Location = new System.Drawing.Point(910, 13);
+            this.bt_Scan.Name = "bt_Scan";
+            this.bt_Scan.Size = new System.Drawing.Size(86, 23);
+            this.bt_Scan.TabIndex = 5;
+            this.bt_Scan.Text = "Scan AT Port";
+            this.bt_Scan.UseVisualStyleBackColor = true;
+            this.bt_Scan.Click += new System.EventHandler(this.bt_Scan_Click);
+            // 
+            // bt_RFTest
+            // 
+            this.bt_RFTest.Location = new System.Drawing.Point(818, 42);
+            this.bt_RFTest.Name = "bt_RFTest";
+            this.bt_RFTest.Size = new System.Drawing.Size(86, 23);
+            this.bt_RFTest.TabIndex = 5;
+            this.bt_RFTest.Text = "RF Test";
+            this.bt_RFTest.UseVisualStyleBackColor = true;
+            this.bt_RFTest.Click += new System.EventHandler(this.bt_RFTest_Click);
+            // 
+            // cb_Port1
+            // 
+            this.cb_Port1.FormattingEnabled = true;
+            this.cb_Port1.Location = new System.Drawing.Point(818, 14);
+            this.cb_Port1.Name = "cb_Port1";
+            this.cb_Port1.Size = new System.Drawing.Size(86, 21);
+            this.cb_Port1.TabIndex = 4;
+            this.cb_Port1.Text = "No COM Port";
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 115200;
+            this.serialPort1.ReadBufferSize = 10240;
+            this.serialPort1.WriteBufferSize = 10240;
+            // 
+            // bt_Download
+            // 
+            this.bt_Download.Location = new System.Drawing.Point(910, 42);
+            this.bt_Download.Name = "bt_Download";
+            this.bt_Download.Size = new System.Drawing.Size(86, 23);
+            this.bt_Download.TabIndex = 5;
+            this.bt_Download.Text = "Download";
+            this.bt_Download.UseVisualStyleBackColor = true;
+            this.bt_Download.Click += new System.EventHandler(this.bt_RFTest_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(1008, 473);
+            this.Controls.Add(this.rtb_Info);
+            this.Controls.Add(this.bt_Scan);
+            this.Controls.Add(this.tabCtrl1);
+            this.Controls.Add(this.bt_Download);
+            this.Controls.Add(this.bt_RFTest);
+            this.Controls.Add(this.cb_Port1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "NB Test Tool";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabCtrl1.ResumeLayout(false);
+            this.tabGraph.ResumeLayout(false);
+            this.tabLog.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabCtrl1;
+        private System.Windows.Forms.TabPage tabGraph;
+        private System.Windows.Forms.Button bt_RFTest;
+        private System.Windows.Forms.ComboBox cb_Port1;
+        private ZedGraph.ZedGraphControl zedGraph;
+        private System.Windows.Forms.TabPage tabLog;
+        private System.Windows.Forms.RichTextBox rtb_Log;
+        private System.Windows.Forms.RichTextBox rtb_Info;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button bt_Scan;
+        private System.Windows.Forms.Button bt_Download;
+    }
+}
+
---
+++ Source/NBST/Form1.cs
@@ -0,0 +1,550 @@
+﻿using System;
+using System.Drawing;
+using System.IO.Ports;
+using System.Threading;
+using System.Management;
+using System.Windows.Forms;
+using System.Reflection;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.Remoting.Contexts;
+
+namespace NBST
+{
+    public partial class Form1 : Form
+    {
+        private static Thread Thread_Task = null;
+        private int DoNext = 0;
+        private string ModuleName = null;
+        private string ModuleImei = null;
+        private StreamWriter sW = null;
+        private string fileName = null;
+        private long line = 0;
+        private string[] UsbPid = new string[3] { 
+        /* xE866 */ "VID_1BC7&PID_0021", 
+        /* MEx10G1 */ "VID_1BC7&PID_110A",
+        /* LE910 */ "VID_1BC7&PID_1201"};
+
+        private long Tick_Get()
+        {
+            return DateTime.Now.Ticks;
+        }
+
+        private long Tick_DifUs(long tk)
+        {
+            return (long)((UInt64)(DateTime.Now.Ticks - tk));
+        }
+
+        private long Tick_DifMs(long tk)
+        {
+            return (long)((UInt64)(DateTime.Now.Ticks - tk) / 1000);
+        }
+
+        private bool Tick_IsOverUs(ref long tk, long ms)
+        {
+            long dif = (long)((UInt64)(DateTime.Now.Ticks - tk));
+
+            if (dif > ms)
+            {
+                tk = Tick_Get();
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool Tick_IsOverMs(ref long tk, long ms)
+        {
+            long dif = (long)((UInt64)(DateTime.Now.Ticks - tk) / 1000);
+
+            if(dif > ms)
+            {
+                tk = Tick_Get();
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool FindString(char c, ref int pIdx, string StrSample)
+        {
+            char[] pStrSample=StrSample.ToCharArray();
+
+            if (c == pStrSample[pIdx])
+            {
+                pIdx++;
+
+                if (pIdx == pStrSample.Length) 
+                {
+                    pIdx = 0;
+                    return true; // matched
+                }
+            }
+            else
+                pIdx = 0;
+
+            return false;
+        }
+
+        private int GetUnixTimeSeconds(DateTime date)
+        {
+            DateTime point = new DateTime(1970, 1, 1);
+            TimeSpan time = date.Subtract(point);
+
+            return (int)time.TotalSeconds;
+        }
+
+        private int ToUnixTimeSeconds()
+        {
+            return GetUnixTimeSeconds(DateTime.Now);
+        }
+
+        private string strRemove(string s)
+        {
+            int i, j, accept;
+            char[] a = s.ToCharArray();
+
+            for (i = 0, accept = 0; i < a.Length; i++)
+            {
+                if (a[i] == '_')
+                    accept++;
+                else if ((a[i] >= '0') && (a[i] <= '9'))
+                    accept++;
+                else if ((a[i] >= 'A') && (a[i] <= 'Z'))
+                    accept++;
+                else if ((a[i] >= 'a') && (a[i] <= 'z'))
+                    accept++;
+            }
+
+            char[] arr = new char[accept];
+
+            for (i = 0, j = 0; i < a.Length; i++)
+            {
+                if (a[i] == '_')
+                    arr[j++] = a[i];
+                else if ((a[i] >= '0') && (a[i] <= '9'))
+                    arr[j++] = a[i];
+                else if ((a[i] >= 'A') && (a[i] <= 'Z'))
+                    arr[j++] = a[i];
+                else if ((a[i] >= 'a') && (a[i] <= 'z'))
+                    arr[j++] = a[i];
+            }
+
+            return new string(arr);
+        }
+
+        private void PrintDebug(string msg, Color color)
+        {
+            rtb_Log.SelectionColor = color;
+            rtb_Log.AppendText(msg);
+            //rtb_Log.ForeColor = color;
+        }
+
+        private void PrintDebug(string msg)
+        {
+            PrintDebug(msg, Color.Black);
+        }
+
+        private void PrintTxDebug(string msg)
+        {
+            PrintDebug(msg, Color.Blue);
+        }
+
+        private void PrintRxDebug(string msg)
+        {
+            PrintDebug(msg, Color.Red);
+        }
+
+        private string FileNameGenerate(string prefix)
+        {
+            string s = strRemove(prefix + DateTime.Now.ToShortDateString() + DateTime.Now.ToLongTimeString());
+
+            return s;
+        }
+
+        private void WriteLogFile(int rsrp, int rsrq)
+        {
+            if (sW != null)
+            {
+                line++;
+
+                string s = line.ToString() + " " + rsrp.ToString() + " " + rsrq.ToString();
+
+                sW.WriteLine(s);
+                PrintDebug("\n" + line.ToString("D4") + " ");
+                PrintDebug(DateTime.Now.ToShortDateString() + ", " + DateTime.Now.ToLongTimeString() + " RSRP=");
+                PrintDebug(rsrp.ToString() + "dBm, " + rsrq.ToString() + " dB");
+            }
+        }
+
+        private void CloseLogFile()
+        {
+            if (sW != null)
+            {
+                StreamWriter slog = new StreamWriter(fileName + ".txt");
+
+                slog.WriteLine(rtb_Log.Text);
+                slog.Close();
+                sW.WriteLine(NBST.Properties.Resources.footer);
+
+                sW.Close();
+                PrintDebug("\nLog file " + fileName + ".m has been saved");
+                Thread.Sleep(1000);
+                rtb_Log.Clear();
+            }
+        }
+
+        private void OpenLogFile()
+        {
+            CloseLogFile();
+            fileName = FileNameGenerate("NBST_" + ModuleName + "_" + ModuleImei + "_");
+
+            try
+            {
+                sW = new StreamWriter(fileName + ".m");
+                sW.WriteLine("%{");
+                sW.WriteLine(rtb_Info.Text);
+                sW.WriteLine("%}");
+                sW.WriteLine(NBST.Properties.Resources.header);
+                sW.WriteLine("\nstartTime=" + ToUnixTimeSeconds().ToString() + ";");
+                sW.WriteLine("\na=[");
+
+                rtb_Log.Text = "New log file " + fileName + ".m has been created";
+                line = 0;
+            }
+            catch (Exception e)
+            {
+                PrintDebug("\nFile name \"" + fileName + "\"error");
+            }
+        }
+
+        private string Parse_COMPort(string str)
+        {
+            // ...(COM10)
+            int i, j, k;
+            string s = null;
+            char[] arr = str.ToCharArray();
+
+            for (i = 0; i < (arr.Length - 4); i++)
+            {
+                if ((arr[i] == 'C') && (arr[i + 1] == 'O') && (arr[i + 2] == 'M'))
+                {
+                    i += 3;
+                    break;
+                }
+            }
+
+            for (j = i, k = 0; j < arr.Length; j++)
+            {
+                if (arr[j] == ')')
+                {
+                    if (j > i)
+                        s = "COM" + k.ToString();
+
+                    break;
+                }
+
+                if ((arr[j] >= '0') && (arr[j] <= '9'))
+                {
+                    k *= 10;
+                    k += (int)(arr[j] - '0');
+                }
+
+            }
+
+            return s;
+        }
+
+        private bool TestATPort(string portName)
+        {
+            int loop = 0;
+            int doNext = 0;
+
+            try
+            {
+                if (serialPort1.IsOpen)
+                    return false;
+
+                serialPort1.PortName = portName;
+                serialPort1.BaudRate = 115200;
+                serialPort1.WriteTimeout = 1000;
+                serialPort1.DtrEnable = true;
+                serialPort1.Open();
+                PrintDebug("\nOpen " + portName);
+
+                int idx = 0;
+                bool first = true;
+                bool found = false;
+                long tick = Tick_Get();
+
+                do
+                {
+                    switch (doNext)
+                    {
+                        case 0:
+                            idx = 0;
+                            doNext++;
+                            serialPort1.WriteLine("AT\r");
+                            PrintTxDebug("\nTX: AT");
+                            break;
+
+                        default:
+                            if (serialPort1.BytesToRead > 0)
+                            {
+                                found = false;
+
+                                if (first == true)
+                                {
+                                    first = false;
+                                    PrintRxDebug("\nRX: ");
+                                }
+
+                                for (int i = 0; i < serialPort1.BytesToRead; i++)
+                                {
+                                    char c = (char)serialPort1.ReadByte();
+
+                                    PrintRxDebug(c.ToString());
+
+                                    if (FindString(c, ref idx, "OK"))
+                                        found = true;
+                                }
+
+                                if (found == true)
+                                {
+                                    serialPort1.DtrEnable = false;
+                                    serialPort1.Close();
+                                    serialPort1.Dispose();
+
+                                    return true;
+                                }
+                            }
+                            else if (Tick_IsOverMs(ref tick, 500))
+                            {
+                                loop++;
+                                doNext = 0;
+                                PrintDebug("\nRX Timeout");
+                            }
+                            break;
+                    }
+                }
+                while (loop < 5);
+            }
+            catch (Exception ex)
+            {
+                //PrintDebug(ex.ToString());
+                PrintDebug("\nTX Timeout");
+            }
+
+            serialPort1.DtrEnable = false;
+            serialPort1.Close();
+            serialPort1.Dispose();
+
+            return false;
+        }
+
+        private void Scan_AT_Port()
+        {
+            string[] ports;
+            int found = 0;
+            string port = null;
+            string portlist = null;
+
+            try
+            {
+                cb_Port1.Items.Clear();
+                cb_Port1.Enabled = false;
+                cb_Port1.Text = "Searching...";
+                ManagementObjectSearcher deviceList = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_PnPEntity WHERE Caption like '%(COM%'");
+
+                if (deviceList != null)
+                {
+                    foreach (ManagementObject device in deviceList.Get())
+                    {
+                        foreach (string Pid in UsbPid) 
+                        {
+                            if (device["DeviceID"].ToString().Contains(Pid))
+                            {
+                                port = Parse_COMPort(device["Caption"].ToString()); // new
+                                //PrintDebug("\nFound " + port);
+
+                                if (portlist == null)
+                                {
+                                    portlist += port;
+                                    //PrintDebug("\nFound: " + port);
+
+                                    if (TestATPort(port))
+                                    {
+                                        found++;
+                                        cb_Port1.Items.Add(port);
+                                    }
+                                }
+                                else if (!portlist.Contains(port))
+                                {
+                                    portlist += port;
+                                    //PrintDebug("\nFound: " + port);
+
+                                    if (TestATPort(port))
+                                    {
+                                        found++;
+                                        cb_Port1.Items.Add(port);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                /*********************************************************************************************************/
+                deviceList = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_POTSModem");
+
+                if (deviceList != null)
+                {
+                    foreach (ManagementObject device in deviceList.Get())
+                    {
+                        port = device["AttachedTo"].ToString();
+                        //PrintDebug("\nFound " + port);
+
+                        if (portlist == null)
+                        {
+                            portlist += port;
+                            //PrintDebug("\nFound: " + port);
+
+                            if (TestATPort(port))
+                            {
+                                found++;
+                                cb_Port1.Items.Add(port);
+                            }
+                        }
+                        else if (!portlist.Contains(port))
+                        {
+                            portlist += port;
+                            //PrintDebug("\nFound: " + port);
+
+                            if (TestATPort(port))
+                            {
+                                found++;
+                                cb_Port1.Items.Add(port);
+                            }
+                        }
+
+                    }
+
+                }
+                /*********************************************************************************************************/
+                if (found == 0)
+                    cb_Port1.Items.Add("Empty");
+
+                cb_Port1.SelectedIndex = 0;
+            }
+            catch (Exception e)
+            {
+                cb_Port1.Items.Add("Empty");
+                cb_Port1.SelectedIndex = 0;
+            }
+
+            cb_Port1.Enabled = true;
+            PrintDebug("\nFound: " + found.ToString() + " AT command port(s)");
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+            this.Text += " v." + $"{version}";
+            this.Update();
+            Scan_AT_Port();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (Thread_Task != null)
+            {
+                if (Thread_Task.IsAlive)
+                {
+                    Thread_Task.Interrupt();
+                    Thread_Task.Abort();
+                    Thread_Task.Join();
+                    while (Thread_Task.IsAlive) ;
+                }
+            }
+
+            CloseLogFile();
+        }
+
+        private void rtb_Log_TextChanged(object sender, EventArgs e)
+        {
+            rtb_Log.SelectionStart = rtb_Log.Text.Length;
+            rtb_Log.ScrollToCaret();
+
+            if (line >= 3600)
+                OpenLogFile();
+        }
+
+        private void bt_Scan_Click(object sender, EventArgs e)
+        {
+            Scan_AT_Port();
+        }
+
+        private void bt_RFTest_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (bt_RFTest.Text == "RF Test")
+                {
+                    DoNext = 0;
+                    serialPort1.PortName = cb_Port1.Text;
+                    serialPort1.BaudRate = 115200;
+                    serialPort1.WriteTimeout = 500;
+                    serialPort1.DtrEnable = true;
+                    serialPort1.Open();
+                    OpenLogFile();
+                    bt_Download.Enabled = false;
+                    bt_Scan.Enabled = false;
+
+                    Thread_Task = new Thread(() => RFTest()); // Create new app tasks
+                    Thread_Task.Start();
+
+                    bt_RFTest.Text = "Stop";
+                }
+                else
+                {
+                    Thread_Task.Interrupt();
+                    Thread_Task.Abort();
+                    Thread_Task.Join();
+                    while (Thread_Task.IsAlive) ;
+
+                    CloseLogFile();
+                    serialPort1.DtrEnable = false;
+                    serialPort1.Close();
+                    serialPort1.Dispose();
+                    bt_Download.Enabled = true;
+                    bt_Scan.Enabled = true;
+                    bt_RFTest.Text = "RF Test";
+                }
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+
+        private void RFTest()
+        {
+
+        }
+
+        private void rtb_Log_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (sW != null)
+                OpenLogFile();
+
+            rtb_Log.Clear();
+        }
+    }
+}
